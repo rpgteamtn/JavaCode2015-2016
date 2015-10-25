3af6b06381dc7d6d783bd0e0586da0327f8591b0
albornzineCahill <legojonah39@gmail.com>
2015-10-25 15:33:00 -0500

    made a section for our opmodes

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -60,14 +60,21 @@ public class FtcOpModeRegister implements OpModeRegister {
      * If two or more op modes are registered with the same name, the app will display an error.
      */
 
-    manager.register("NullOp", NullOp.class);
-
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    //**************Team RPG's opModes******************\\
     manager.register("DriveTrain", DriveTrain.class);
-    manager.register("TestBot", TestBot.class);
+    //manager.register("TestBot", TestBot.class);
+
+
+
+
+
+   // manager.register("NullOp", NullOp.class);
+
+   // manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
+   // manager.register ("PushBotAuto", PushBotAuto.class);
+    //manager.register ("PushBotManual", PushBotManual.class);
+
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
