39c7f530e4ea41aa02b84e71e9380add0cf4e14f
albornzineCahill <legojonah39@gmail.com>
2015-10-31 10:47:21 -0500

    commented the drive driver

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DriveDriver.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DriveDriver.java
@@ -36,14 +36,18 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+//This program keeps the functions for the drive train
+
 public class DriveDriver
 {
-  DcMotor driveMotorLB;
-  DcMotor driveMotorLF;
-  DcMotor driveMotorRB;
-  DcMotor driveMotorRF;
+  //Define the motors
+  DcMotor driveMotorLB;//LB  is left back
+  DcMotor driveMotorLF;//LF  is left front
+  DcMotor driveMotorRB;//RB is Right Back
+  DcMotor driveMotorRF;//RF  is right front
 
-
+  // this FUnction allows other programs to use the drive driver functions
   public DriveDriver(DcMotor lb, DcMotor lf, DcMotor rb, DcMotor rf)
   {
       driveMotorLB = lb;
@@ -52,15 +56,17 @@ public class DriveDriver
       driveMotorRF = rf;
   }
 
+  //set motors get inputs of the left and right power and sends commands
   public void setMotors(double leftPower, double rightPower)
   {
-    driveMotorRF.setPower(rightPower);
-    driveMotorRB.setPower(rightPower);
-    driveMotorLF.setPower(leftPower);
-    driveMotorLB.setPower(leftPower);
+    driveMotorRF.setPower(rightPower);//sets RF as the right power
+    driveMotorRB.setPower(rightPower);//sets RB as the right power
+    driveMotorLF.setPower(leftPower);//sets LF as the left power
+    driveMotorLB.setPower(leftPower);//sets LB as the left power
   }
 
-
+//this function gets an input of the distance you want to travel IN INCHES and returns
+// the number of clicks that the encoder needs to travel
   public double driveEncoderMath(int Dist)
   {
 
@@ -75,7 +81,7 @@ public class DriveDriver
     return finalDist;
   }
 
-
+//this function resets the encoders to 0
   public void resetEncoders()
   {
     driveMotorLB.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
@@ -84,22 +90,25 @@ public class DriveDriver
     driveMotorRB.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
   }
 
-
+//gets inputs of the distance IN INCHES and power and goes the distance you set at the power you set
   public void moveDist(int Dist, double power)
   {
-    double distToGo = driveEncoderMath(Dist);
-    resetEncoders();
+    double distToGo = driveEncoderMath(Dist);// uses the Function driveEncoderMath to convert inchs to clicks
+    resetEncoders();// sets the encoders to 0 so that we get an accurate distance
 
+    //sets target possistion of each encoder
     driveMotorLB.setTargetPosition((int) distToGo);
     driveMotorLF.setTargetPosition((int) distToGo);
     driveMotorRB.setTargetPosition((int) distToGo);
     driveMotorRF.setTargetPosition((int) distToGo);
 
+    //sets the run mode of the motors to the target positions
     driveMotorLB.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
     driveMotorLF.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
     driveMotorRB.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
     driveMotorRF.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
+    //uses the function setMotors to set the motors to the power inputed into the function
     setMotors(power, power);
   }
 
