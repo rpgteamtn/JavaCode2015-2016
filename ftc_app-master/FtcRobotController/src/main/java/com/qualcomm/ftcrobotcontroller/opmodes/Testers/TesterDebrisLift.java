8b10b010eed6c6aea13ea31a4c5d415bfb2c16ed
ckonjeti <ckonjeti1@gmail.com>
2015-11-21 14:30:46 -0600

    Tester for debris lift

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testers/TesterDebrisLift.java
@@ -0,0 +1,14 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Testers;
+
+import com.qualcomm.ftcrobotcontroller.opmodes.DebrisLift;
+
+/**
+ * Created by Chaitu on 21-11-2015.
+ */
+public class TesterDebrisLift{
+
+    public void DebrisLiftTester(){
+        for(int i = 1; i < 6; i++){
+        debrisLiftMotor.setPower(1);
+    }
+}
