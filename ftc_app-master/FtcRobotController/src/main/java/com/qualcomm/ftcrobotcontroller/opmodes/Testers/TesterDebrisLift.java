e45a78493521c2298cd26ad7bbdfbc0a4c0c2312
ckonjeti <ckonjeti1@gmail.com>
2015-11-21 15:05:21 -0600

    someone fix the sleep error thingy things pls help me im dying :(

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testers/TesterDebrisLift.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testers/TesterDebrisLift.java
@@ -1,18 +1,19 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.Testers;
 
 import com.qualcomm.ftcrobotcontroller.opmodes.DebrisLift;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 /**
  * Created by Chaitu on 21-11-2015.
  */
-public class TesterDebrisLift {
+public class TesterDebrisLift extends LinearOpMode {
     DebrisLift DebrisLift;
 
     public void DebrisLiftTester() {
         DebrisLift.debrisLiftMotor.setPower(1);
-        DebrisLift.debrisLiftMotor.sleep(10000);
+        sleep(10000);
         DebrisLift.debrisLiftMotor.setPower(-1);
-
+        sleep(10000);
 
     }
 }
