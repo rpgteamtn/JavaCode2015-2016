35825ed501cd4157e6364302f69a040f8072ef0c
Natalie Rose Hershey <natalieluvsunicorns@gmail.com>
2015-12-19 12:46:10 -0600

    I made a thing

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testers/TesterMotors.java
@@ -0,0 +1,24 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Testers;
+
+/**
+ * Created by Natalie on 12/19/2015.
+ */
+
+import com.qualcomm.ftcrobotcontroller.opmodes.Drivers.DriveDriver;
+import com.qualcomm.ftcrobotcontroller.opmodes.Drivers.Timer;
+
+public class TesterMotors {
+    DriveDriver DriveDriver;
+    Timer Timer
+
+    public void TestMotors()
+       {
+
+           DriveDriver.setMotors(0.7, 0.7);
+           Timer.Sleep(2000);
+
+       }
+
+
+
+}
