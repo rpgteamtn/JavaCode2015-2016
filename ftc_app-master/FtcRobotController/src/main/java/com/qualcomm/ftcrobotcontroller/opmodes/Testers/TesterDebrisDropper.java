2caac58d61d6e87f3695e0602ad86b8bd6049520
Josh416 <Joshcrabtree416@gmail.com>
2016-01-09 11:24:55 -0600

    Added Comments

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testers/TesterDebrisDropper.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testers/TesterDebrisDropper.java
@@ -5,14 +5,18 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class TesterDebrisDropper
 {
+    // Connects DebrisDrop to DcMotor function
     DcMotor DebrisDropperMotor;
 
     public void DebrisDropperTester()
     {
+        // Motors drop debris
+        // Drops for 5 seconds then stops
         DebrisDropperMotor.setPower(1);
         Timer.sleep(5000);
         DebrisDropperMotor.setPower(-1);
         Timer.sleep(5000);
     }
+    // Allows RunTester to call upon program
     public TesterDebrisDropper(){}
 }
